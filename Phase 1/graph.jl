24d61b3199d09c69a1506d31c05510e245f61194
nathanemac <91251698+nathanemac@users.noreply.github.com>
2023-10-02 09:10:28 -0400

    Initial commit

---
+++ Phase 1/graph.jl
@@ -0,0 +1,46 @@
+import Base.show
+
+"""Type abstrait dont d'autres types de graphes dériveront."""
+abstract type AbstractGraph{T} end
+
+"""Type representant un graphe comme un ensemble de noeuds.
+
+Exemple :
+
+    node1 = Node("Joe", 3.14)
+    node2 = Node("Steve", exp(1))
+    node3 = Node("Jill", 4.12)
+    G = Graph("Ick", [node1, node2, node3])
+
+Attention, tous les noeuds doivent avoir des données de même type.
+"""
+mutable struct Graph{T} <: AbstractGraph{T}
+  name::String
+  nodes::Vector{Node{T}}
+end
+
+"""Ajoute un noeud au graphe."""
+function add_node!(graph::Graph{T}, node::Node{T}) where T
+  push!(graph.nodes, node)
+  graph
+end
+
+# on présume que tous les graphes dérivant d'AbstractGraph
+# posséderont des champs `name` et `nodes`.
+
+"""Renvoie le nom du graphe."""
+name(graph::AbstractGraph) = graph.name
+
+"""Renvoie la liste des noeuds du graphe."""
+nodes(graph::AbstractGraph) = graph.nodes
+
+"""Renvoie le nombre de noeuds du graphe."""
+nb_nodes(graph::AbstractGraph) = length(graph.nodes)
+
+"""Affiche un graphe"""
+function show(graph::Graph)
+  println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes.")
+  for node in nodes(graph)
+    show(node)
+  end
+end
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projet MTH6412
+ 
