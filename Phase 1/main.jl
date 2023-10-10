533c25f11825051a7ae314d1b25769d5df89ce18
Vicdar2477 <115122223+Vicdar2477@users.noreply.github.com>
2023-10-10 01:55:30 +0000

    add_isempty(nodes_graph) condition

--- Phase 1/main.jl
+++ Phase 1/main.jl
@@ -6,6 +6,22 @@ include("edge.jl")
 
 
 """Construit un graphe à partir d'une instance TSP symétrique dont les poids sont donnés au format EXPLICIT et renvoie l'objet Graph associé"""
+#  Arguments
+# - `instance::String` : Le chemin vers le fichier TSP contenant les données de l'instance.
+# - `title::String` : Le titre du graphe construit.
+# Examples
+```
+julia> build_graph("instances/stsp/bays29.tsp", "Graph_Test")
+
+Graph Graph_Test has 29 nodes and 435 edges.
+Nodes:
+Node 1, data: [1150.0, 1760.0]
+Node 2, data: [630.0, 1660.0]
+# ... (d'autres nœuds et arêtes)
+Edge from 28 to 29, weight: 199.0
+Edge from 29 to 29, weight: 0.0
+```
+
 function build_graph(instance::String, title::String)
   
   header = read_header(instance)
@@ -21,6 +37,14 @@ function build_graph(instance::String, title::String)
   nodes_graph = read_nodes(header, instance)
   edges_graph = read_edges(header, instance)
 
+  if isempty(nodes_graph)
+    for i = 1:parse(Int64, header["DIMENSION"])
+      dict_intermediaire = Dict(i => [])
+      merge!(nodes_graph, dict_intermediaire)
+    end
+  end
+
+
   graph_built = ExtendedGraph(title, [Node("1", nodes_graph[1])])
   for i=2:length(nodes_graph)
     node = Node(string(i), nodes_graph[i])
@@ -33,7 +57,6 @@ function build_graph(instance::String, title::String)
     w_edge = edges_graph[i][3]
     add_edge!(graph_built, s_node, e_node, w_edge)
   end
-
   show(graph_built)
 end
 
