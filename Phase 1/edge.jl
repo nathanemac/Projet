198f9d1a359d585369b372746e4454ab241f3211
nathanemac <91251698+nathanemac@users.noreply.github.com>
2023-10-23 09:32:37 -0400

    push

--- Phase 1/edge.jl
+++ Phase 1/edge.jl
@@ -178,3 +178,23 @@ function read_edges(header::Dict{String, String}, filename::String)
   close(file)
   return edges
 end
+
+"""
+Structure contenant les composantes connexes d'un graphe.
+
+Exemple:
+
+    node1 = Node("Joe", 3.14)
+    node2 = Node("Steve", exp(1))
+    edge = Edge(node1, node2)       # Arête non pondérée
+    edge = Edge(node1, node2, 5.6)  # Arête pondérée avec un poids de 5.6
+"""
+mutable struct ConnexGraph{T} <: AbstractGraph{T}
+  name::String
+  nodes::Vector{Node{T}}
+  edges::Vector{Edge{T}}
+end
+
+
+# Constructeur pour ExtendedGraph
+ConnexGraph(name::String, nodes::Vector{Node{T}}) where T = ConnexGraph(name, nodes, Edge{T}[])
